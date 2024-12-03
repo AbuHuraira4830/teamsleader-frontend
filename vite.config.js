fb8db12beb3b4d3120bcf86d6806b12423db526c
Usman <usmanhaider36104@gmail.com>
2024-12-03 08:21:51 +0500

    merging code

--- vite.config.js
+++ vite.config.js
@@ -1,47 +1,61 @@
 // import { defineConfig } from "vite";
 // import react from "@vitejs/plugin-react";
-// import { createRequire } from 'node:module';
+// import { createRequire } from "node:module";
 // const require = createRequire(import.meta.url);
-// import ckeditor5 from '@ckeditor/vite-plugin-ckeditor5';
+// import ckeditor5 from "@ckeditor/vite-plugin-ckeditor5";
+// import { resolve } from "path";
 
 // // https://vitejs.dev/config/
 // export default defineConfig({
 //   plugins: [
 //     react(),
 //     ckeditor5({
-//       theme: require.resolve('@ckeditor/ckeditor5-theme-lark')
-//     })
+//       theme: require.resolve("@ckeditor/ckeditor5-theme-lark"),
+//     }),
 //   ],
 //   optimizeDeps: {
-//     include: ['tailwindcss'],
+//     include: ["tailwindcss"],
+//   },
+//   resolve: {
+//     alias: {
+//       "react-dnd": resolve(__dirname, "node_modules/react-dnd"),
+//       "react-dnd-html5-backend": resolve(
+//         __dirname,
+//         "node_modules/react-dnd-html5-backend"
+//       ),
+//     },
 //   },
 // });
 
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { createRequire } from "node:module";
-const require = createRequire(import.meta.url);
+import { createRequire } from "node:module"; // Import createRequire
+const require = createRequire(import.meta.url); // Create a CommonJS-like require
 import ckeditor5 from "@ckeditor/vite-plugin-ckeditor5";
 import { resolve } from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     ckeditor5({
-      theme: require.resolve("@ckeditor/ckeditor5-theme-lark"),
+      theme: require.resolve("@ckeditor/ckeditor5-theme-lark"), // Use the created require
     }),
   ],
   optimizeDeps: {
-    include: ["tailwindcss"],
+    exclude: [
+      "@ckeditor/ckeditor5-core",
+      "@ckeditor/ckeditor5-editor-classic",
+      "@ckeditor/ckeditor5-table",
+      "@ckeditor/ckeditor5-basic-styles",
+      "@ckeditor/ckeditor5-upload",
+    ],
   },
   resolve: {
     alias: {
-      "react-dnd": resolve(__dirname, "node_modules/react-dnd"),
-      "react-dnd-html5-backend": resolve(
-        __dirname,
-        "node_modules/react-dnd-html5-backend"
-      ),
+      "@ckeditor": resolve(__dirname, "node_modules/@ckeditor"),
     },
   },
 });
+
+
+

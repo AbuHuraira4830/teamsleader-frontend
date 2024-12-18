a9e9e02fe2ec417501ebaf5ac91da9bf00113044
Usman <usmanhaider36104@gmail.com>
2024-12-18 11:46:16 +0500

    alert bug fix

--- vite.config.js
+++ vite.config.js
@@ -4,17 +4,10 @@ import { createRequire } from "node:module"; // Import createRequire
 const require = createRequire(import.meta.url); // Create a CommonJS-like require
 import ckeditor5 from "@ckeditor/vite-plugin-ckeditor5";
 import { resolve } from "path";
-import { visualizer } from "rollup-plugin-visualizer";
 
 export default defineConfig({
   plugins: [
     react(),
-    visualizer({
-      open: true, // Automatically open the report in your browser
-      filename: "bundle-report.html", // Customize the file name (optional)
-      // Other options as needed (see below)   
-    }),
-
     ckeditor5({
       theme: require.resolve("@ckeditor/ckeditor5-theme-lark"), // Use the created require
     }),

cd7b980e1bf6e60241d135fb8cebd5e1e8e5c7f4
Huraira <huraira.dogar14@gmail.com>
2025-04-17 02:24:33 -0700

    adding backend link

--- src/helpers/apis.js
+++ src/helpers/apis.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:8888";
+// axios.defaults.baseURL = "http://localhost:8888";
+axios.defaults.baseURL = "https://teaamsleader-backend.onrender.com/";
 // axios.defaults.baseURL =
 //   "https://miketeamsleaderbackend-a03d0e00169c.herokuapp.com";
 // axios.defaults.headers.common["Authorization"] =
